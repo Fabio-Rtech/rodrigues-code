e96f2c912fbcc1bcb93d770fe257edd1d7cdade9
Fabio-Rtech <f42rodrigues@gmail.com>
2026-07-02 01:27:50 -0300

    Delete Old-School Barber-Shop/Old-School Barber-Shop directory

--- Old-School Barber-Shop/Old-School Barber-Shop/menu.js
+++
@@ -1,5 +0,0 @@
-/* Efeito de rolagem na navBar/menu */
-window.addEventListener("scroll", function () {
-    let header= document.querySelector('#header');
-    header.classList.toggle('rolagem', window.scrollY > 0);
-})
